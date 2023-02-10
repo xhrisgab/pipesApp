5ea658bf3c1ebca82c9c9b067e58207e1b55f6e0
xhrisgab <xhris.gab@gmail.com>
2023-02-10 18:29:28 -0400

    fin configuracion de rutas y directorios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'Christian CccCc';
+  numero: number = 1000;
+
+  getNombre(){
+    console.log(this.nombre);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Modulo Personalizado
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouterModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
